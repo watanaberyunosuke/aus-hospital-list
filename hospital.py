2ee4078ad7f825cc0c90575c7bb8be46d59659c5
watanaberyunosuke <harrybwatson@protonmail.com>
2024-07-23 23:46:43 +1000

    Update format and main

--- hospital.py
+++ hospital.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String
 from database import Base
 
+
 class Hospital(Base):
     __tablename__ = "Australia_Hospital"
     objectid = Column(Integer)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import os
 from fastapi import FastAPI
 from database import Base, engine
 
-import hospital
 import route
 
 Base.metadata.create_all(bind=engine)
@@ -9,6 +9,13 @@ app = FastAPI()
 
 app.include_router(router=route.router)
 
+
 @app.get("/")
 async def root():
     return {"message": "fastapi"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
